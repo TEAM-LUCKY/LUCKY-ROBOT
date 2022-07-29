f63ddd9b32d7ba52387e1aaa9c089e8cc5304c3a
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-29 23:39:06 +0530

    Update antinsfw.py

--- EmikoRobot/modules/antinsfw.py
+++ EmikoRobot/modules/antinsfw.py
@@ -7,7 +7,7 @@ from EmikoRobot.utils.errors import capture_err
 from EmikoRobot.utils.permissions import adminsOnly
 from EmikoRobot.ex_plugins.dbfunctions import is_nsfw_on, nsfw_off, nsfw_on
 
-__mod_name__ = "Anti-NSFW​"
+__mod_name__ = "ᴀɴᴛɪ-ɴꜱꜰᴡ​"
 
 
 async def get_file_id_from_message(message):
