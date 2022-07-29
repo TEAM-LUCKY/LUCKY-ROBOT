3152a2874cd6d78bb2f903ec9e0e035004dc36b1
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-30 00:42:35 +0530

    Update myanimelist.py

--- EmikoRobot/modules/myanimelist.py
+++ EmikoRobot/modules/myanimelist.py
@@ -188,7 +188,7 @@ def manga(update: Update, context: CallbackContext):
         )
 
 
-__mod_name__ = "MyAnimeList"
+__mod_name__ = "ᴀɴɪᴍᴇʟɪꜱᴛ"
 
 
 ANIME_HANDLER = CommandHandler("manime", anime, pass_args=True, run_async=True)
