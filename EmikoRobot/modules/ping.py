54198a3e7357a5e33bce98569f9dc495af65cf45
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-30 14:13:08 +0530

    Update ping.py

--- EmikoRobot/modules/ping.py
+++ EmikoRobot/modules/ping.py
@@ -79,9 +79,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "<b>PONG</b> ✨\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>ᴘɪɴɢɪɴɢ ʙᴀʙʏ</b> 🍒\n"
+        "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
+        "<b>ꜱᴇʀᴠɪᴇᴄᴇ ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
