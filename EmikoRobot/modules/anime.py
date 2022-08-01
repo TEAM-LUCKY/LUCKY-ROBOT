081931c7e244493f1f64c458abda038b4fadd8d6
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-01 11:55:17 +0530

    Update anime.py

--- EmikoRobot/modules/anime.py
+++ EmikoRobot/modules/anime.py
@@ -600,7 +600,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "ᴀɴɪᴍᴇ"
+__mod_name__ = "Aɴɪᴍᴇ 🍂"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
     "kayo"
