0dcaa2358e3571dab015ad2bb22015365c4599c9
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-01 15:44:20 +0530

    Update mongo.py

--- EmikoRobot/mongo.py
+++ EmikoRobot/mongo.py
@@ -7,11 +7,10 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
 from EmikoRobot import MONGO_DB_URI
-from EmikoRobot.confing import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_PORT = "27017"
+MONGO_DB_URI = "MONGO_DB_URI"
 MONGO_DB = "EmikoRobot"
 
 
