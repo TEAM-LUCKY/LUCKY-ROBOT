744c772e60be164e79304e94c34c5227589f3d20
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-01 14:59:28 +0530

    Update config.py

--- EmikoRobot/config.py
+++ EmikoRobot/config.py
@@ -1,6 +1,11 @@
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
+    def get_user_list(config, key):
+    with open("{}/EmikoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+class Config(object):
 
     LOGGER = True
     # REQUIRED
