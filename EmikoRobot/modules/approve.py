f3f51aaf2f8f2eac16a67be580422585461a6004
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-02 09:31:11 +0530

    Update approve.py

--- EmikoRobot/modules/approve.py
+++ EmikoRobot/modules/approve.py
@@ -228,6 +228,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Aᴘᴘʀᴏᴠᴇʟꜱ 💠"
+__mod_name__ = "Aᴘᴘʀᴏᴠᴇ 💠"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
