b03d31e4729b42bf6c96e742090c2c39d5e7339a
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-11 09:38:50 +0530

    Update global_bans.py

--- EmikoRobot/modules/global_bans.py
+++ EmikoRobot/modules/global_bans.py
@@ -560,7 +560,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "Aɴᴛɪ-ꜱᴘᴀᴍ 🔥"
+__mod_name__ = "Sᴘᴀᴍ ♦️"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
