1eb7b350860f1b5c4bff181eefdf450d49acac87
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-13 23:04:37 +0530

    Update misc.py

--- EmikoRobot/modules/helper_funcs/misc.py
+++ EmikoRobot/modules/helper_funcs/misc.py
@@ -88,12 +88,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 8 : 8 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "°☜°", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "⟲", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• °ʙᴀᴄᴋ° •", callback_data="source_back"),
-                ),
+                EqInlineKeyboardButton("• ɢᴏ ʜᴏᴍᴇ •", callback_data="source_back"),
                 EqInlineKeyboardButton(
-                    "°☞°", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "⟳", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
