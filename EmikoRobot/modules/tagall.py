eda2472f51a9225cb46da27a18ee779516fdc601
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-13 12:02:34 +0530

    Update tagall.py

--- EmikoRobot/modules/tagall.py
+++ EmikoRobot/modules/tagall.py
@@ -109,7 +109,7 @@ async def cancel_spam(event):
         return await event.respond("__Stopped Mention.__")
 
 
-__mod_name__ = "ᴛᴀɢᴀʟʟ"
+__mod_name__ = "Tᴀɢᴀʟʟ🥴"
 __help__ = """
 ──「 Mention all func 」──
 
