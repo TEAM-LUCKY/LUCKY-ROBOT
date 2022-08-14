dfa8ca7d9fd9611bbf54341272912b322054ee09
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-14 13:07:20 +0530

    Update welcome.py

--- EmikoRobot/modules/welcome.py
+++ EmikoRobot/modules/welcome.py
@@ -1328,7 +1328,7 @@ dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 dispatcher.add_handler(CAPTCHA_BUTTON_VERIFY_HANDLER)
 
-__mod_name__ = "Wᴇʟᴄᴏᴍᴇ 😍"
+__mod_name__ = "𝚆ʟᴄᴍ 😍"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
